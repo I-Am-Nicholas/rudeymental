9cc2f686c98f191c358008f44cc9b5cf3c0ab4e2
Nicholas Leacock <nicholas@swissmail.com>
2017-06-05 00:02:49 +0100

    Add routing test. Add test for new controller method.

--- spec/units/cusswords_controller_spec.rb
+++ spec/units/cusswords_controller_spec.rb
@@ -9,10 +9,19 @@ describe CusswordsController do
       should route(:get, '/').
       to(page: :home, controller: :cusswords, action: :index)
     end
+
+    it do
+      should route(:get, '/cusswords/show').
+      to(controller: :cusswords, action: :show, id: :show)
+    end
   end
 
   it "can receive messages from the show method" do
     expect(described_class.new).to respond_to(:show)
+  end
+
+  it "can receive messages from the words method" do
+    expect(described_class.new).to respond_to(:words)
   end
 
 end
